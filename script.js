e17dc9a17734f914b1e814cd38406082b82f380b
Rakin1205 <141386625+Rakin1205@users.noreply.github.com>
2023-09-12 20:43:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ fetch('intents.json')
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    let chatContent = className === "outgoing" ? `<p></p>` : `<p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi;
@@ -50,7 +50,7 @@ const handleChat = () => {
     
     setTimeout(() => {
         const response = generateResponse(userMessage);
-        const incomingChatLi = createChatLi(response, "incoming");
+        const incomingChatLi = createChatLi(response,"incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
     }, 600);
